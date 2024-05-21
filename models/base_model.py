432b04fed0e66ad1e9ee4f7cc32a9c35955fe5f3
Jenniscott <scottbellojennifer@gmail.com>
2024-05-21 04:14:42 -0700

    ok

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime and saves the instance."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance."""
+        dict_repr = self.__dict__.copy()
+        dict_repr["__class__"] = self.__class__.__name__
+        dict_repr["created_at"] = self.created_at.isoformat()
+        dict_repr["updated_at"] = self.updated_at.isoformat()
+        return dict_repr
